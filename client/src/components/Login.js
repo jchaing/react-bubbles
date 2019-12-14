145c15c4c9f798de0ae581f3985270c70d472f2b
john <john@spinningplates.org>
2019-12-13 18:12:58 -0800

    Axios request for token after login, save token to localStorage

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,55 @@
-import React from "react";
+import React, {useState} from 'react';
+import axios from 'axios';
 
-const Login = () => {
+const Login = props => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [credentials, setCredentials] = useState({
+    username: '',
+    password: ''
+  });
+
+  const handleChange = e => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value
+    });
+    console.log(credentials);
+  };
+
+  const login = e => {
+    e.preventDefault();
+    axios
+      .post('http://localhost:5000/api/login', credentials)
+      .then(res => {
+        console.log('Login.js login: res', res);
+        localStorage.setItem('token', res.data.payload);
+        props.history.push('/protected');
+      })
+      .catch(err => console.log(err));
+  };
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
       <p>Build a login page here</p>
+      <form onSubmit={login}>
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          value={credentials.username}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={credentials.password}
+          onChange={handleChange}
+        />
+        <button>Submit</button>
+      </form>
     </>
   );
 };
